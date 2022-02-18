1ee8827384042a7502a6602793f5e974a0cc6cec
Alexander Dimitryuk <dimitryuk38@gmail.com>
2022-02-18 18:48:01 +0200

    66 done

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import { applyMiddleware } from "redux";
+import  thunkMiddleware from 'redux-thunk'
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -14,7 +15,7 @@ let reducers = combineReducers({
     auth: authReducer
 });
 
-let store = createStore(reducers, applyMiddleware);
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
