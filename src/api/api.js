07fc2bd49b5cdaa86d7a17a1fa71b4e965486940
Dimitryuk <79190561+Dimitryuk@users.noreply.github.com>
2022-03-08 16:41:42 +0200

    67 lesson done

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,35 @@
 import * as axios from "axios";
 
-
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers:     {
-        "API-KEY": "137e9872-a15e-4330-bd36-6b8fe8f40ad2"
-    }
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "137e9872-a15e-4330-bd36-6b8fe8f40ad2",
+  },
 });
 
-
 export const usersAPI = {
-    getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(response => {
-                return response.data;
-            });
-    },
-    follow(userId) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
-    },
-    unfollow(userId) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
-    }
-}
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+  follow(userId) {
+    return instance.post(`follow/${userId}`);
+  },
+  unfollow(userId) {
+    return instance.delete(`follow/${userId}`);
+  },
 
+  getProfile(userId) {
+    return instance.get(`profile/` + userId);
+  },
+};
 
+export const authAPI = {
+  me() {
+    return instance.get(`auth/me`);
+  },
+};
